8f0ee00984440632eb55e2076f836e18c6503d2f
SHIVASHANKAR ENAGANDULA <136105695+shivashankarenagandula@users.noreply.github.com>
2026-07-13 12:05:16 +0530

    Delete apps directory

--- apps/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import RegisterParentView, RegisterTutorView
-
-urlpatterns = [
-    path("register/parent/", RegisterParentView.as_view(), name="register-parent"),
-    path("register/tutor/", RegisterTutorView.as_view(), name="register-tutor"),
-]
