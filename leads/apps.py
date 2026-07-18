471f5fc0b6e7194287678e0e9c0cbfafa10f12df
SHIVASHANKAR ENAGANDULA <136105695+shivashankarenagandula@users.noreply.github.com>
2026-07-18 11:00:51 +0530

    Delete leads directory

--- leads/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class LeadsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.leads"
-
-    def ready(self):
-        import apps.leads.signals  # noqa: F401
