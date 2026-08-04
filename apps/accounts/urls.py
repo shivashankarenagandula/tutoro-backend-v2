af791c203697ddf2e663be7acc3d452f400ac333
SHIVASHANKAR ENAGANDULA <136105695+shivashankarenagandula@users.noreply.github.com>
2026-08-04 18:33:49 +0530

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import RegisterParentView, RegisterTutorView
+from .views import RegisterParentView, RegisterTutorView, RequestOTPView, VerifyOTPView
 
 urlpatterns = [
     path("register/parent/", RegisterParentView.as_view(), name="register-parent"),
     path("register/tutor/", RegisterTutorView.as_view(), name="register-tutor"),
+    path("otp/request/", RequestOTPView.as_view(), name="otp-request"),
+    path("otp/verify/", VerifyOTPView.as_view(), name="otp-verify"),
 ]
